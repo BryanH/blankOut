4d901bec10bbee94fa28170ce85c5234381af9bc
Bryan Hanks, PMP CSP-SM CSPO <github@master-developer.com>
2026-05-18 13:42:19 -0500

    Cleaned up code, fixed manifest

--- content_script.js
+++ content_script.js
@@ -1,26 +1,29 @@
 function removeBlankTargets(root = document) {
-  root.querySelectorAll?.('a[target="_blank"]').forEach((anchor) => {
-    anchor.removeAttribute("target");
-  });
+    root.querySelectorAll?.('a[target="_blank"]')
+        .forEach((anchor) => {
+            anchor.removeAttribute("target");
+        });
 }
 
 removeBlankTargets();
 
 /* Check if document was updated (by a script or something) */
 const observer = new MutationObserver((mutations) => {
-  for (const mutation of mutations) {
-    for (const node of mutation.addedNodes) {
-      if (node.nodeType === Node.ELEMENT_NODE) {
-        if (node.matches?.('a[target="_blank"]')) {
-          node.removeAttribute("target");
+    for (const mutation of mutations) {
+        for (const node of mutation.addedNodes) {
+            if (node.nodeType === Node.ELEMENT_NODE) {
+                if (node.matches?.('a[target="_blank"]')) {
+                    node.removeAttribute("target");
+                }
+                removeBlankTargets(node);
+                //var keys = Object.keys(node);
+                console.log("Removed blank target from ["+node+"] [" + Object.keys(node) + "]");
+            }
         }
-        removeBlankTargets(node);
-      }
     }
-  }
 });
 
 observer.observe(document.documentElement, {
-  childList: true,
-  subtree: true
+    childList: true,
+    subtree: true
 });
